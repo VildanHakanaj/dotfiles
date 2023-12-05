2e6f38ba84b038cb2420d899427d85500f266432
Vildan Hakanaj <vildanhakanaj@gmail.com>
2023-12-04 23:21:20 -0500

    Update aliases tmux and add t script

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # Set dot file path
 DOTFILES="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+rm -rf $HOME/.config/kitty
+ln -s $DOTFILES/kitty $HOME/.config/kitty
+
 ln -sf $DOTFILES/zsh/.zshrc $HOME/.zshrc
 
 ln -sf $DOTFILES/zsh/.zsh_aliases $HOME/.zsh_aliases
@@ -13,4 +16,5 @@ ln -sf $DOTFILES/nvim $HOME/.config/nvim
 rm -rf $HOME/bin
 ln -s $DOTFILES/bin $HOME/bin
 
+rm -rf $HOME/.tmux.conf
 ln -sf $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -43,4 +43,3 @@ source $ZSH/oh-my-zsh.sh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
